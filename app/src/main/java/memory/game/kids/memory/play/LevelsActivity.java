ec380a95281b82d4825b3fc06f9bed4419eb0761
ShiryAbdo <shaimaaenfikry@gmail.com>
2017-11-09 12:52:25 +0200

    Initial commit

---
+++ app/src/main/java/memory/game/kids/memory/play/LevelsActivity.java
@@ -0,0 +1,16 @@
+package memory.game.kids.memory.play;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import memory.game.kids.memory.R;
+
+
+public class LevelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels);
+    }
+}
